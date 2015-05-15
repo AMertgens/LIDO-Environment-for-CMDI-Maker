0280dde6b084a771275d300a3694124c62180a70
Sebastian Zimmer <sebastian.zimmer@uni-koeln.de>
2015-05-15 13:42:58 +0200

    Erwerb-Form + Inszenierungs-Form

--- src/js/lido_forms.js
+++ src/js/lido_forms.js
@@ -197,3 +197,155 @@ lido_environment.forms.actor_herstellung = {
 		}
 	]
 };
+
+
+
+lido_environment.forms.inszenierung = {
+	name: "inszenierung",
+	title: "Inszenierung / Performance",
+	type: "form",
+	fields: [
+		{
+			name: "titel",
+			heading: "Titel",
+			type: "text"
+		}
+		{
+			heading: "Performance-Beschreibung",
+			name: "performancebeschreibung",
+			type: "textarea"
+		},
+		{
+			heading: "Datum (frühestes)",
+			name: "earliest_date",
+			type: "year"
+		},
+		{
+			heading: "Datum (spätestes)",
+			name: "latest_date",
+			type: "year"
+		},
+		{
+			heading: "Kultureller Kontext",
+			name: "culture"
+			type: "text"
+		}
+	]
+};
+
+
+lido_environment.forms.actor_inszenierung = {
+	name: "actor",
+	type: "form",
+	fields: [
+		{
+			name: "name",
+			heading: "Name (Name, Vorname)",
+			type: "text",
+		},
+		{
+			name: "function",
+			heading: "Funktion",
+			type: "open_vocabulary",
+			vocabulary: [
+				"AutorIn des inszenierten Werks", "BühnenbildnerIn der Inszenierung", "ChoreographIn des inszenierten Werks",
+				"DarstellerIn der Inszenierung", "DrehbuchautorIn des Films", "KomponistIn des inszenierten Werks",
+				"Konzeptionierung", "RegisseurIn des inszenierten Werks"
+			]
+		},
+		{
+			name: "pnd_id",
+			heading: "AkteurIn ID (PNG)",
+			type: "text"
+		},
+		{
+			name: "geburtsjahr",
+			heading: "Geburtsjahr",
+			type: "year"
+		},
+		{
+			name: "sterbejahr",
+			heading: "Sterbejahr",
+			type: "year"
+		},
+		{
+			name: "geschlecht",
+			heading: "Geschlecht",
+			type: "select",
+			vocabulary: ["male", "female"]
+		}
+	]
+};
+
+
+
+
+lido_environment.forms.erwerb = {
+	name: "erwerb",
+	title: "Provenienz / Erwerb",
+	type: "form",
+	fields: [
+		{
+			heading: "Provenienzbeschreibung",
+			name: "provenienzbeschreibung",
+			type: "textarea"
+		},
+		{
+			heading: "Datum (frühestes)",
+			name: "earliest_date",
+			type: "year"
+		},
+		{
+			heading: "Datum (spätestes)",
+			name: "latest_date",
+			type: "year"
+		},
+		{
+			heading: "Kultureller Kontext",
+			name: "culture"
+			type: "text"
+		}
+	]
+};
+
+
+lido_environment.forms.actor_erwerb = {
+	name: "actor",
+	type: "form",
+	fields: [
+		{
+			name: "name",
+			heading: "Name (Name, Vorname)",
+			type: "text",
+		},
+		{
+			name: "function",
+			heading: "Funktion",
+			type: "open_vocabulary",
+			vocabulary: [
+				"KäuferIn", "NachlasserIn", "StifterIn", "VerkäuferIn", "VermittlerIn", "VorbesitzerIn"
+			]
+		},
+		{
+			name: "pnd_id",
+			heading: "AkteurIn ID (PNG)",
+			type: "text"
+		},
+		{
+			name: "geburtsjahr",
+			heading: "Geburtsjahr",
+			type: "year"
+		},
+		{
+			name: "sterbejahr",
+			heading: "Sterbejahr",
+			type: "year"
+		},
+		{
+			name: "geschlecht",
+			heading: "Geschlecht",
+			type: "select",
+			vocabulary: ["male", "female"]
+		}
+	]
+};
